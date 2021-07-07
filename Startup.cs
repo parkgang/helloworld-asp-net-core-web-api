7a1ce3c65a532d3f0dd7dd72d136d0bad0cc1062
박경은 <ruddms936@naver.com>
2021-07-07 23:43:34 +0900

    add: 기본적인 crud 컨트롤러 추가

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using HelloWorldAspNetCoreWebApi.Models;
 using Microsoft.OpenApi.Models;
 
 namespace HelloWorldAspNetCoreWebApi
@@ -23,7 +18,7 @@ namespace HelloWorldAspNetCoreWebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // 이 메서드는 런타임에 의해 호출됩니다. 이 방법을 사용하여 컨테이너에 서비스를 추가합니다.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -32,9 +27,11 @@ namespace HelloWorldAspNetCoreWebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HelloWorldAspNetCoreWebApi", Version = "v1" });
             });
+            // DI 컨테이너에 데이터베이스 컨텍스트를 추가 및 데이터베이스 컨텍스트가 메모리 내 데이터베이스를 사용하도록 지정
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // 이 메서드는 런타임에 의해 호출됩니다. 이 방법을 사용하여 HTTP 요청 파이프 라인을 구성합니다.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
